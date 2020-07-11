3a4756022694acfce0e9dd2c006c64fee8fe2c1c
unknown <weirfreddie@gmail.com>
2020-07-11 14:19:11 +0100

    smol changes

--- economy.js
+++ economy.js
@@ -2,13 +2,17 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('../config.json');
 const token = config.token;
+const PREFIX = config.prefix;
+const BOT_ID = config.bot_id;
+const FRODO_ID = config.frodo_id;
+var userVars = {};
 
 bot.on('ready', () => {
     console.log("Economy Online (running via parent script)");
 });
 
-bot.on("message", function(msg){
-    if (msg.content == "test") {
+bot.on("message", function(msg) {
+    if (msg.content == "--") {
         msg.channel.send("success!");
     }
 });
